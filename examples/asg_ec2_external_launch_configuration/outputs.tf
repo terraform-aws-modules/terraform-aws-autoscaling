d5e5703fca79188a5cdd18e6ff98fc6bc19953f8
Anton Babenko <anton@antonbabenko.com>
2018-08-18 20:38:10 +0200

    Fix to support externally created launch configurations (fix #43)

---
+++ examples/asg_ec2_external_launch_configuration/outputs.tf
@@ -0,0 +1,9 @@
+output "this_launch_configuration_id" {
+  description = "The ID of the launch configuration"
+  value       = "${module.example.this_launch_configuration_id}"
+}
+
+output "this_autoscaling_group_id" {
+  description = "The autoscaling group id"
+  value       = "${module.example.this_autoscaling_group_id}"
+}
