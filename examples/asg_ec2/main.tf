b15ad306b478d8eb934a330c61e0a69d91d1a440
amandasoaares <amanda14_sc@hotmail.com>
2020-05-14 17:22:16 -0300

    delete resource linked service role

--- examples/asg_ec2/main.tf
+++ examples/asg_ec2/main.tf
@@ -33,24 +33,6 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_iam_service_linked_role" "autoscaling" {
-  aws_service_name = "autoscaling.amazonaws.com"
-  description      = "A service linked role for autoscaling"
-  custom_suffix    = "something"
-
-  # Sometimes good sleep is required to have some IAM resources created before they can be used
-  provisioner "local-exec" {
-    command = "sleep 10"
-  }
-}
-
-locals {
-  user_data = <<EOF
-#!/bin/bash
-echo "Hello Terraform!"
-EOF
-}
-
 ######
 # Launch configuration and autoscaling group
 ######
