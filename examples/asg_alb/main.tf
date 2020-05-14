c1bae4d10f9daf67a5dfe81de75df5eca4050bb8
amandasoaares <amanda14_sc@hotmail.com>
2020-05-14 16:33:57 -0300

    fix vpc range and variable name

--- examples/asg_alb/main.tf
+++ examples/asg_alb/main.tf
@@ -8,12 +8,12 @@ provider "aws" {
 module "vpc" {
   source = "github.com/youse-seguradora/terraform-aws-vpc"
 
-  name = "teste"
+  name = var.vpc_name
 
   cidr = "10.120.0.0/24"
 
   azs                    = ["us-east-2a"]
-  compute_public_subnets = ["10.120.0.2.0/24"]
+  compute_public_subnets = ["10.120.2.0/24"]
 }
 
 data "aws_security_group" "default" {
@@ -38,7 +38,7 @@ data "aws_ami" "amazon_linux" {
 module "example_asg" {
   source = "../../"
 
-  name = "example-with-elb"
+  name = var.asg_name
 
 
   lc_name = var.lc_name
