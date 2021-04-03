79cee63dd0eb94d1feea66d7453b3586cb5cc233
Bryant Biggs <bryantbiggs@gmail.com>
2021-04-03 08:39:22 -0400

    chore: update example for initial lifecycle hooks

---
+++ examples/inital_lifecycle_hook/main.tf
@@ -0,0 +1,162 @@
+provider "aws" {
+  region = local.region
+
+  # Make it faster by skipping something
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+locals {
+  name   = "example-initial-lifecycle-hook"
+  region = "eu-west-1"
+
+  tags = [
+    {
+      key                 = "Project"
+      value               = "megasecret"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "foo"
+      value               = ""
+      propagate_at_launch = true
+    },
+  ]
+
+  tags_as_map = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  user_data = <<-EOT
+  #!/bin/bash
+  echo "Hello Terraform!"
+  EOT
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 2"
+
+  name = local.name
+  cidr = "10.99.0.0/18"
+
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets  = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
+  private_subnets = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = local.tags_as_map
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+}
+
+################################################################################
+# Launch configuration
+################################################################################
+
+module "launch_configuration" {
+  source = "../../"
+
+  # Autoscaling group
+  name = "lc-${local.name}"
+
+  vpc_zone_identifier = module.vpc.private_subnets
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
+
+  initial_lifecycle_hooks = [
+    {
+      name                 = "ExampleStartupLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 60
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                 = "ExampleTerminationLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 180
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "goodbye" = "world" })
+    }
+  ]
+
+  # Launch configuration
+  use_lc    = true
+  create_lc = true
+
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  tags        = local.tags
+  tags_as_map = local.tags_as_map
+}
+
+################################################################################
+# Launch template
+################################################################################
+
+module "launch_template" {
+  source = "../../"
+
+  # Autoscaling group
+  name = "lt-${local.name}"
+
+  vpc_zone_identifier = module.vpc.private_subnets
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
+
+  initial_lifecycle_hooks = [
+    {
+      name                 = "ExampleStartupLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 60
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                 = "ExampleTerminationLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 180
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "goodbye" = "world" })
+    }
+  ]
+
+  # Launch template
+  use_lt    = true
+  create_lt = true
+
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  tags        = local.tags
+  tags_as_map = local.tags_as_map
+}
