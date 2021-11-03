5b0b367f466c029d39188367203fe065a099d58e
Juan A. Moyano <wincus@gmail.com>
2021-11-03 13:46:06 -0300

    fix max_instance_lifetime min and max values in description

--- variables.tf
+++ variables.tf
@@ -152,7 +152,7 @@ variable "suspended_processes" {
 }
 
 variable "max_instance_lifetime" {
-  description = "The maximum amount of time, in seconds, that an instance can be in service, values must be either equal to 0 or between 604800 and 31536000 seconds"
+  description = "The maximum amount of time, in seconds, that an instance can be in service, values must be either equal to 0 or between 86400 and 31536000 seconds"
   type        = number
   default     = null
 }
